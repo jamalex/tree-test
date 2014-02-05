b4de96551fc44572d69316e7ab8be23b2bda0b0c
Jamie Alexandre <jamalex@gmail.com>
2014-02-05 07:55:24 -0800

    Renamed json1_file to json_file

--- tree/tree/project_views.py
+++ tree/tree/project_views.py
@@ -10,9 +10,9 @@ def load(request, addr):
 
     slug_list = [slug for slug in addr.strip('/').split("/") if slug]
 
-    with open('json1.json', 'r') as json1_file:
+    with open('json1.json', 'r') as json_file:
 
-        node = {"children": [json.load(json1_file)], "kind": "Topic", "title": "Content"}
+        node = {"children": [json.load(json_file)], "kind": "Topic", "title": "Content"}
 
         for slug in slug_list:
             for child in node.get("children", []):
