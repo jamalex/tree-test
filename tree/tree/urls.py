d57a7c77abd1318ce4d9dda8d0ff92d6fc76b7e3
codemania113 <paridhi2011@gmail.com>
2014-02-05 20:02:26 +0530

    new code

--- tree/tree/urls.py
+++ tree/tree/urls.py
@@ -1,14 +1,15 @@
+import settings
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tree.project_views import load
-import settings
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-    url(r'^stories-and-literature/$', load, {'addr': 'stories-and-literature'}),
-    url(r'stories-and-literature/([a-zA-Z0-9_.-]+/)*(?P<addr>[a-zA-Z0-9_.-]+)/$', load, {}),
+    url(r'(?P<addr>[^\f]+)/$', load, {}),
 )
 
